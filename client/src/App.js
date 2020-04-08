6feb22438c065bc1e904d5870adcd6e06ea1d445
jc <jcmujica@gmail.com>
2020-04-07 20:58:29 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
